9a8ec40068602c4f56dac9ccb1a9cc0d97d33b1e
rakshi0230 <tadikamallarakshitha@gmail.com>
2024-11-28 20:44:56 +0530

    first modification

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
 
-"""Top-level script to invoke helloworld implementation."""
 
-import sys
-import helloworld.main
 
-if __name__ == '__main__':
-    sys.exit(helloworld.main.main())
+
+
+
+
+"""this is a python programming"""
+
